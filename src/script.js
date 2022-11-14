f8746700477b9960e5032efdd6048f5a1424bba0
Santiago <santiagov801@gmail.com>
2022-11-13 19:59:40 -0500

    model import

---
+++ src/script.js
@@ -0,0 +1,145 @@
+import './style.css';
+import * as dat from 'lil-gui';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI({
+  width: 400,
+});
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Loaders
+ */
+// Texture loader
+const textureLoader = new THREE.TextureLoader();
+
+// Draco loader
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('draco/');
+
+// GLTF loader
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+
+//Textures
+const bakedTexture = textureLoader.load('baked.jpg');
+bakedTexture.flipY = false;
+bakedTexture.encoding = THREE.sRGBEncoding;
+
+//Baked material
+const bakedMaterial = new THREE.MeshBasicMaterial({
+  map: bakedTexture,
+});
+
+//Pole lights Material
+const poleLightMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffe5,
+});
+
+//Portal Light Material
+const portalLightMaterial = new THREE.MeshBasicMaterial({ color: 'lightblue' });
+
+//Blender Scene
+gltfLoader.load('mergedPortal.glb', (gltf) => {
+  const bakedMesh = gltf.scene.children.find((item) => item.name === 'baked');
+  const poleLightRightMesh = gltf.scene.children.find(
+    (item) => item.name === 'poleLightRight'
+  );
+  const poleLightLeftMesh = gltf.scene.children.find(
+    (item) => item.name === 'poleLightLeft'
+  );
+  const portalLightMesh = gltf.scene.children.find(
+    (item) => item.name === 'Circle'
+  );
+  bakedMesh.material = bakedMaterial;
+  poleLightRightMesh.material = poleLightMaterial;
+  poleLightLeftMesh.material = poleLightMaterial;
+  portalLightMesh.material = portalLightMaterial;
+  portalLightMesh.material.side = THREE.DoubleSide;
+
+  scene.add(gltf.scene);
+});
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 4;
+camera.position.y = 2;
+camera.position.z = 4;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
